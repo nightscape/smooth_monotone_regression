6f08b6112eec8f880cbd8e928a45a03c4e8a998a
Martin Mauch <mauch@crealytics.de>
2014-09-08 10:51:39 +0200

    Use bounds of already found interval.

--- src/main/scala/ISplineFromR.scala
+++ src/main/scala/ISplineFromR.scala
@@ -23,18 +23,18 @@ class ISplineFromR(t: DenseVector[Double]) {
     val Interval(index, left, right) = knots.intervalOf(x)
     val sigma = DenseVector.tabulate(m) {
       case (0) => 1.0
-      case (j) if j == k + 1 && index == 0 => 1 - scala.math.pow(x - t(1), 2) / (t(1) - t(0)) / (t(1) - t(0))
+      case (j) if j == k + 1 && index == 0 => 1 - scala.math.pow(x - right, 2) / (right - left) / (right - left)
       case (j) if j == k + 1 && index >= 1 => 1.0
       case (j) if j == k + 1 && index == k + 2 => 1.0
       case (j) if j == k + 2 && index < k => 0.0
-      case (j) if j == k + 2 && index == k => scala.math.pow(x - t(k), 2) / (t(k + 1) - t(k)) / (t(k + 1) - t(k))
+      case (j) if j == k + 2 && index == k => scala.math.pow(x - left, 2) / (right - left) / (right - left)
       case (j) if j == k + 2 && index >= k => 1.0
       case (j) if j == k && index < k - 1 => 0.0
-      case (j) if j == k && index == k - 1 => scala.math.pow(x - t(k - 1), 2) / (t(k + 1) - t(k - 1)) / (t(k) - t(k - 1))
-      case (j) if j == k && index == k => 1 - scala.math.pow(x - t(k + 1), 2) / (t(k + 1) - t(k)) / (t(k + 1) - t(k - 1))
+      case (j) if j == k && index == k - 1 => scala.math.pow(x - left, 2) / (knots(k + 1) - left) / (right - left)
+      case (j) if j == k && index == k => 1 - scala.math.pow(x - right, 2) / (right - left) / (right - knots(k - 1))
       case (j) if index < j - 1 => 0.0
-      case (j) if index == j - 1 => scala.math.pow(x - t(j - 1), 2) / (t(j + 1) - t(j - 1)) / (t(j) - t(j - 1))
-      case (j) if index == j => 1 - scala.math.pow(x - t(j + 1), 2) / (t(j + 1) - t(j)) / (t(j + 1) - t(j - 1))
+      case (j) if index == j - 1 => scala.math.pow(x - left, 2) / (knots(j + 1) - left) / (right - left)
+      case (j) if index == j => 1 - scala.math.pow(x - right, 2) / (right - left) / (right - knots(j - 1))
       case (j) if index >= j + 1 => 1.0
       case _ => throw new RuntimeException("Should not have gotten here")
     }
