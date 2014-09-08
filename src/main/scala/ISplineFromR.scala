03fbda639111a46a85a220bca69e48b3cea1ef3e
Martin Mauch <mauch@crealytics.de>
2014-09-08 10:41:14 +0200

    Create method for calculating coefficients for a single x.

--- src/main/scala/ISplineFromR.scala
+++ src/main/scala/ISplineFromR.scala
@@ -2,27 +2,12 @@ import breeze.linalg.DenseMatrix
 import breeze.linalg.DenseVector
 
 class ISplineFromR(t: DenseVector[Double]) {
+  val knots = new ArbitrarilyDistributedKnots(t.toArray)
   def monotoneIncreasing(x: DenseVector[Double]): DenseMatrix[Double] = {
     val n = x.length
     val k = t.length - 2
     val m = k + 3
-    val sigma = DenseMatrix.tabulate(m, n) {
-      case (0, _) => 1.0
-      case (j, r) if j == k + 1 && x(r) <= t(1) => 1 - scala.math.pow(x(r) - t(1), 2) / (t(1) - t(0)) / (t(1) - t(0))
-      case (j, r) if j == k + 1 && x(r) > t(1) => 1.0
-      case (j, r) if j == k + 1 && x(r) > t(k + 2) => 1.0
-      case (j, r) if j == k + 2 && x(r) <= t(k) => 0.0
-      case (j, r) if j == k + 2 && x(r) > t(k) && x(r) <= t(k + 1) => scala.math.pow(x(r) - t(k), 2) / (t(k + 1) - t(k)) / (t(k + 1) - t(k))
-      case (j, r) if j == k + 2 && x(r) > t(k + 1) => 1.0
-      case (j, r) if j == k && x(r) <= t(k - 1) => 0.0
-      case (j, r) if j == k && x(r) > t(k - 1) && x(r) <= t(k) => scala.math.pow(x(r) - t(k - 1), 2) / (t(k + 1) - t(k - 1)) / (t(k) - t(k - 1))
-      case (j, r) if j == k && x(r) > t(k) && x(r) <= t(k + 1) => 1 - scala.math.pow(x(r) - t(k + 1), 2) / (t(k + 1) - t(k)) / (t(k + 1) - t(k - 1))
-      case (j, r) if x(r) <= t(j - 1) => 0.0
-      case (j, r) if x(r) > t(j - 1) && x(r) <= t(j) => scala.math.pow(x(r) - t(j - 1), 2) / (t(j + 1) - t(j - 1)) / (t(j) - t(j - 1))
-      case (j, r) if x(r) > t(j) && x(r) <= t(j + 1) => 1 - scala.math.pow(x(r) - t(j + 1), 2) / (t(j + 1) - t(j)) / (t(j + 1) - t(j - 1))
-      case (j, r) if x(r) > t(j+1) => 1.0
-      case _ => throw new RuntimeException("Should not have gotten here")
-    }
+    val sigma = DenseMatrix.horzcat(x.toArray.map(monIncr).map(_.toDenseMatrix.t): _*)
 
     for (i <- 1 until m) {
       val sigs = sigma(i, ::)
@@ -30,5 +15,29 @@ class ISplineFromR(t: DenseVector[Double]) {
       sigs := sigs - ms
     }
     sigma
+  }
+  def monIncr(x: Double): DenseVector[Double] = {
+    val k = t.length - 2
+    val m = k + 3
+    val Interval(index, left, right) = knots.intervalOf(x)
+    val sigma = DenseVector.tabulate(m) {
+      case (0) => 1.0
+      case (j) if j == k + 1 && index == 0 => 1 - scala.math.pow(x - t(1), 2) / (t(1) - t(0)) / (t(1) - t(0))
+      case (j) if j == k + 1 && index >= 1 => 1.0
+      case (j) if j == k + 1 && index == k + 2 => 1.0
+      case (j) if j == k + 2 && index < k => 0.0
+      case (j) if j == k + 2 && index == k => scala.math.pow(x - t(k), 2) / (t(k + 1) - t(k)) / (t(k + 1) - t(k))
+      case (j) if j == k + 2 && index >= k => 1.0
+      case (j) if j == k && index < k - 1 => 0.0
+      case (j) if j == k && index == k - 1 => scala.math.pow(x - t(k - 1), 2) / (t(k + 1) - t(k - 1)) / (t(k) - t(k - 1))
+      case (j) if j == k && index == k => 1 - scala.math.pow(x - t(k + 1), 2) / (t(k + 1) - t(k)) / (t(k + 1) - t(k - 1))
+      case (j) if index < j - 1 => 0.0
+      case (j) if index == j - 1 => scala.math.pow(x - t(j - 1), 2) / (t(j + 1) - t(j - 1)) / (t(j) - t(j - 1))
+      case (j) if index == j => 1 - scala.math.pow(x - t(j + 1), 2) / (t(j + 1) - t(j)) / (t(j + 1) - t(j - 1))
+      case (j) if index >= j + 1 => 1.0
+      case _ => throw new RuntimeException("Should not have gotten here")
+    }
+    sigma
+
   }
 }
