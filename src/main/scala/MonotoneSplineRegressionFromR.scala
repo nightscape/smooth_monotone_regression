67bf7bdf3f3d96f57a090fdf61ce0b38c0969571
Martin Mauch <mauch@crealytics.de>
2014-09-11 12:20:05 +0200

    Allow specifying type of monotonicity.

--- src/main/scala/MonotoneSplineRegressionFromR.scala
+++ src/main/scala/MonotoneSplineRegressionFromR.scala
@@ -1,12 +1,11 @@
-import breeze.linalg.DenseVector
-import breeze.linalg.DenseMatrix
+import breeze.linalg._
 
-class MonotoneSplineRegressionFromR(knots: Knots) {
+class MonotoneSplineRegressionFromR(knots: Knots, monotonicity: Int = 1) {
   val splines = new ISplineFromR(knots)
   val optimizer = new HingeQuadraticOptimization
   def learn(xs: DenseVector[Double], ys: DenseVector[Double]): Double => Double = {
     val edges = splines.monotoneIncreasing(xs)
-    val amat = DenseMatrix.eye[Double](knots.size + 1)
+    val amat = diag(DenseVector.fill[Double](knots.size + 1, monotonicity))
     amat.update(0, 0, 0.0)
     val qmat = edges * edges.t
     val cvec = edges * ys
